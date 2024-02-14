d356278c6f2f77e71b19c10b1fb59eb13b648eb2
ishaparmarrnw <158278579+ishaparmarrnw@users.noreply.github.com>
2024-02-14 17:54:47 +0530

    Delete lecture 8.1 directory

--- lecture 8.1/Q1.C
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int number;
-    printf("enetr any number:");
-    scanf("%d",&number);
-    (number%2==0) ? printf(" the even number. ") : printf(" the odd number.");
-    return 0;
-}
