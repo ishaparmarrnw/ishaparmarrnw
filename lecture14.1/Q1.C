22f842908caa707c05b6bd7ebd9d1c0301ef9fad
ishaparmarrnw <158278579+ishaparmarrnw@users.noreply.github.com>
2024-02-29 21:09:15 +0530

    Add files via upload

---
+++ lecture14.1/Q1.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() 
+{
+    int size;
+    printf("Enter the array's size: ");
+    scanf("%d",&size);
+    int arr[size];
+    printf("Enter array's elements:\n");
+    for ( int i = 0; i < size; i++) 
+    {
+        printf("a[%d]=", i);
+        scanf("%d", &arr[i]);
+    }
+    printf("negative elements from array:\n");
+    for ( int i = 0; i < size; i++) 
+    {
+        if (arr[i] < 0) 
+        {
+            printf("%d ", arr[i]);
+        }
+    }
+     return 0;
+}  
+    
